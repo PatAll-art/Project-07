1d23e72a0b334527fd708f183eddf5dc8534f8d6
Patricia <patricia.allande@gmail.com>
2022-11-20 22:07:29 -0500

    added signup.vue

---
+++ my-first-vue-cli-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+   
+    <SignUp/>
+  </div>
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue'
+export default {
+  name: 'App',
+  components: {
+    SignUp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  align-items: center;
+  color: #2c3e50;
+  font-size: 12px;
+  border-bottom: 2px;
+  border-style: black;
+}
+
+
+
+
+</style>
---
+++ my-first-vue-cli-app/src/components/SignUp.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="signUp">
+    <img class="logo" src="../assets/icon-left-font-monochrome-white.svg"/>
+    <h1>Sign Up</h1>
+    <div class="registerAcc">
+        <input type="text" placeholder="Username"/>
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Password"/>
+        <button>Enter</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style lang="scss">
+#signUp {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: center;
+}
+.logo {
+    width: 50%;
+    background-color: #152147;
+    block-size: 100px;
+    padding: 9px 100%;
+}
+
+
+.registerAcc input {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: space-evenly;
+    border: solid darkgrey 1px;
+    border-radius: 30px;
+    padding: 12px;
+    width: 400px;
+    height: 20px;
+    margin-bottom: 20px;
+}
+.registerAcc button {
+    border: none;
+    border-radius: 30px;
+    padding: 30px;
+    width: 300px;
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+    background-color: darkgrey;
+    align-items: center;
+} 
+
+.registerAcc {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
